a00cbc0a62df0b4c98549395d94c4ec7d56b1c7b
Chris Assigbe <cka@cka.dev>
2023-01-10 17:32:54 -0500

    fix client profile images not loading
    Bug: 224772895
    Change-Id: Ic6aff896316d8c93a59212d634d59d1d9b70bc15

--- ClassyTaxiServer/src/controller/functions/content.ts
+++ ClassyTaxiServer/src/controller/functions/content.ts
@@ -14,102 +14,106 @@
  * limitations under the License.
  */
 
- import * as functions from 'firebase-functions';
- import { playBilling, verifyAuthentication, contentManager, verifyFirebaseAuthIdToken, sendHttpsError, verifyInstanceIdToken } from '../shared'
+import * as functions from 'firebase-functions';
+import { SubscriptionStatus } from '../../model/SubscriptionStatus';
+import { playBilling, verifyAuthentication, contentManager, verifyFirebaseAuthIdToken, sendHttpsError, verifyInstanceIdToken } from '../shared'
 
- const BASIC_PLAN_PRODUCT = functions.config().app.basic_plan_product;
- const PREMIUM_PLAN_PRODUCT = functions.config().app.premium_plan_product;
+const BASIC_PLAN_PRODUCT = functions.config().app.basic_plan_product;
+const PREMIUM_PLAN_PRODUCT = functions.config().app.premium_plan_product;
 
- /* This file contains implementation of functions related to content serving.
-  * Each functions checks if the active user have access to the subscribed content,
-  * and then returns the content to client app.
-  */
+/* This file contains implementation of functions related to content serving.
+* Each functions checks if the active user have access to the subscribed content,
+* and then returns the content to client app.
+*/
 
 
- /* Callable that serves basic content to the client
-  */
- export const content_basic = functions.https.onCall(async (data, context) => {
-   verifyAuthentication(context);
-   await verifySubscriptionOwnershipAsync(context, [BASIC_PLAN_PRODUCT, PREMIUM_PLAN_PRODUCT]);
+/* Callable that serves basic content to the client
+*/
+export const content_basic = functions.https.onCall(async (data, context) => {
+  verifyAuthentication(context);
+  await verifySubscriptionOwnershipAsync(context, [BASIC_PLAN_PRODUCT, PREMIUM_PLAN_PRODUCT]);
 
-   return contentManager.getBasicContent()
- })
+  return contentManager.getBasicContent()
+})
 
- /* HTTPS request that serves basic content to the client.
-  *
-  * @param {Request} request
-  * @param {Response} response
-  */
- export const content_basic_v2 = functions.https.onRequest(async (request, response) => {
-   return verifyFirebaseAuthIdToken(request, response)
-      .then(async (decodedToken) => {
-        const uid = decodedToken.uid;
-        await verifySubscriptionOwnershipAsyncV2(uid, [BASIC_PLAN_PRODUCT, PREMIUM_PLAN_PRODUCT]);
+/* HTTPS request that serves basic content to the client.
+*
+* @param {Request} request
+* @param {Response} response
+*/
+export const content_basic_v2 = functions.https.onRequest(async (request, response) => {
+  return verifyFirebaseAuthIdToken(request, response)
+    .then(async (decodedToken) => {
+      const uid = decodedToken.uid;
+      await verifySubscriptionOwnershipAsyncV2(uid, [BASIC_PLAN_PRODUCT, PREMIUM_PLAN_PRODUCT]);
 
-        const data = contentManager.getBasicContent();
-        response.send(data);
-      }).catch((error: functions.https.HttpsError) =>{
-         sendHttpsError(error, response);
-      });
- });
+      const data = contentManager.getBasicContent();
+      response.send(data);
+    }).catch((error: functions.https.HttpsError) => {
+      sendHttpsError(error, response);
+    });
+});
 
- /* Callable that serves premium content to the client
-  */
- export const content_premium = functions.https.onCall(async (data, context) => {
-   verifyAuthentication(context);
-   await verifySubscriptionOwnershipAsync(context, [PREMIUM_PLAN_PRODUCT]);
+/* Callable that serves premium content to the client
+*/
+export const content_premium = functions.https.onCall(async (data, context) => {
+  verifyAuthentication(context);
+  await verifySubscriptionOwnershipAsync(context, [PREMIUM_PLAN_PRODUCT]);
 
-   return contentManager.getPremiumContent()
- });
+  return contentManager.getPremiumContent()
+});
 
- /* HTTPS request that serves premium content to the client
-  *
-  * @param {Request} request
-  * @param {Response} response
-  */
- export const content_premium_v2 = functions.https.onRequest(async (request, response) =>{
-   return verifyFirebaseAuthIdToken(request, response)
-     .then(async (decodedToken) => {
-       const uid = decodedToken.uid;
-       await verifySubscriptionOwnershipAsyncV2(uid, [PREMIUM_PLAN_PRODUCT]);
+/* HTTPS request that serves premium content to the client
+*
+* @param {Request} request
+* @param {Response} response
+*/
+export const content_premium_v2 = functions.https.onRequest(async (request, response) => {
+  return verifyFirebaseAuthIdToken(request, response)
+    .then(async (decodedToken) => {
+      const uid = decodedToken.uid;
+      await verifySubscriptionOwnershipAsyncV2(uid, [PREMIUM_PLAN_PRODUCT]);
 
-       const data = contentManager.getPremiumContent();
-       response.send(data);
-     }).catch((error: functions.https.HttpsError) =>{
-        sendHttpsError(error, response)
-     });
- });
+      const data = contentManager.getPremiumContent();
+      response.send(data);
+    }).catch((error: functions.https.HttpsError) => {
+      sendHttpsError(error, response)
+    });
+});
 
- /* Util function that verifies if current user owns at least one active purchases listed in products
-  */
- async function verifySubscriptionOwnershipAsyncV2(uid: string, products: Array<string>): Promise<void> {
-   const purchaseList = await playBilling.users().queryCurrentSubscriptions(uid)
-     .catch(err => {
-       console.error(err.message);
-       throw new functions.https.HttpsError('internal', 'Internal server error');
-     });
+/* Util function that verifies if current user owns at least one active purchases listed in products
+*/
+async function verifySubscriptionOwnershipAsyncV2(uid: string, products: Array<string>): Promise<void> {
+  const purchaseList = await playBilling.users().queryCurrentSubscriptions(uid)
+    .catch(err => {
+      console.error(err.message);
+      throw new functions.https.HttpsError('internal', 'Internal server error');
+    });
 
-   const isUserHavingTheProduct = purchaseList.some(purchase => ((products.indexOf(purchase.product) > -1) && (purchase.isEntitlementActive())));
-   if (!isUserHavingTheProduct) {
-     throw new functions.https.HttpsError('permission-denied', 'Valid subscription not found');
-   }
- }
+  const subscriptionStatusList = purchaseList.map(subscriptionPurchase => new SubscriptionStatus(subscriptionPurchase));
 
- /*Util function that verifies if current user owns at least one active purchases listed in products
-  *
-  * @param {string} uid
-  * @param {Array<string>} products
-  * @throws {HttpsError}
-  */
- async function verifySubscriptionOwnershipAsync(context: functions.https.CallableContext, products: Array<string>): Promise<void> {
-   const purchaseList = await playBilling.users().queryCurrentSubscriptions(context.auth.uid)
-     .catch(err => {
-       console.error(err.message);
-       throw new functions.https.HttpsError('internal', 'Internal server error');
-     });
+  const doesUserHaveTheProduct = subscriptionStatusList.some(subscription => ((Object.keys(subscription).length > 0) && (subscription.isEntitlementActive)));
 
-   const isUserHavingTheProduct = purchaseList.some(purchase => ((products.indexOf(purchase.product) > -1) && (purchase.isEntitlementActive())));
-   if (!isUserHavingTheProduct) {
-     throw new functions.https.HttpsError('permission-denied', 'Valid subscription not found');
-   }
- }
+  if (!doesUserHaveTheProduct) {
+    throw new functions.https.HttpsError('permission-denied', 'Valid subscription not found');
+  }
+}
+
+/*Util function that verifies if current user owns at least one active purchases listed in products
+*
+* @param {string} uid
+* @param {Array<string>} products
+* @throws {HttpsError}
+*/
+async function verifySubscriptionOwnershipAsync(context: functions.https.CallableContext, products: Array<string>): Promise<void> {
+  const purchaseList = await playBilling.users().queryCurrentSubscriptions(context.auth.uid)
+    .catch(err => {
+      console.error(err.message);
+      throw new functions.https.HttpsError('internal', 'Internal server error');
+    });
+
+  const isUserHavingTheProduct = purchaseList.some(purchase => ((products.indexOf(purchase.product) > -1) && (purchase.isEntitlementActive())));
+  if (!isUserHavingTheProduct) {
+    throw new functions.https.HttpsError('permission-denied', 'Valid subscription not found');
+  }
+}
